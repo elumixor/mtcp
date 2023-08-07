623348fb6b632d851bba610a875294dd8f5835fb
Vladyslav Yazykov <vladyslav.yazykov@cern.ch>
2023-08-07 12:25:06 +0200

    Hot fixes

---
+++ jobs/trex-sys/job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /eos/user/v/vyazykov/eos/TRExFitter/setup.sh
+
+export MTCP_ROOT="/eos/user/v/vyazykov/mtcp"
+cd $MTCP_ROOT/trex-fitter
+
+config="configs/probs-sys.config"
+
+trex-fitter n $config
+trex-fitter w $config
+trex-fitter d $config
+trex-fitter f $config
+trex-fitter r $config
---
+++ jobs/trex-sys/submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set -e
+# set -x
+
+my_path=$(dirname $(realpath $0))
+cd $my_path
+
+export MTCP_ROOT=/eos/user/v/vyazykov/mtcp
+export MTCP_JOB="trex-sys"
+
+afs_job_dir="/afs/cern.ch/user/v/vyazykov/.condor/$MTCP_JOB"
+rm -rf $afs_job_dir
+mkdir -p $afs_job_dir/logs/{out,err,log}
+rm -rf logs
+ln -s $afs_job_dir/logs
+cp -t $afs_job_dir job.sh condor_submit.sub
+cd $afs_job_dir
+
+condor_submit condor_submit.sub
